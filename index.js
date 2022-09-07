0f9eb7766174f1001cf4a44b1c8212d5ea8bbcd1
Adhikari <sanjay.adhikari@aremedia.com.au>
2022-09-07 16:51:29 +1000

    Template setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const { join } = require( 'path' );
+
+module.exports = {
+    module.exports = {
+        defaultValues: {
+            slug: 'custom-block',
+            namespace: "njw",
+            author: "SNJ",
+            category: 'text',
+            title: 'NJWCustom Block ',
+            description:'A custom Gutenberg block to show your pride! This block enables you to type text and style it with the color font Gilbert from Type with Pride.',
+            dashicon: 'hammer',
+            version: '0.1.0',
+        },
+        variants: {
+            static: {},
+            dynamic: {
+                attributes: {
+                    message: {
+                        type: 'string',
+                    },
+                },
+            },
+        },
+        pluginTemplatesPath: join( __dirname, 'plugin-templates' ),
+        blockTemplatesPath: join( __dirname, 'block-templates' ),
+        assetsPath: join( __dirname, 'assets' ),
+    },
+   
+};
